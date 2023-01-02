58944a6f6b1feb40741316bf83a4b6f75ba73744
LamMR5 <asd5213080@gmail.com>
2023-01-02 22:33:28 +0000

    Saved progress on ForLoop 2023-01-02 22:33:28 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -28,10 +28,11 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
+        String arraytoString ="";
         for(int i = 0; i < nums.length; i++){
             
         
-        return nums;}
+        return nums;
     }
 
         
