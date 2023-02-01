2373cc2450a0f1b66eca267a99cb3a0de503b68f
LamMR5 <asd5213080@gmail.com>
2023-02-01 20:44:47 +0000

    Saved progress on ArraysAreEqual 2023-02-01 20:44:47 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -4,7 +4,9 @@ public class DoesNotContain {
      * The inverse of the contains value challenge: check if the array does *not* contain the value.
      * Note that this is not as simple as flipping the return true/false from the 'Contains' challenge.
      * You should think critically about the logic that you write within the for loop.
-     *
+     * (why the logic here should not be flipping the F and T?
+     *                what should be think critically?
+     *                  what else can i do                                                )
      * For a trickier problem like this, you could even try explaining your thought process of working
      * through this problem with pencil and paper in plain English before attempting it in code.
      *
