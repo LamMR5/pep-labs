d5a9cc0ebcbb3ec2e44625d799313fb4b9d0ea78
LamMR5 <asd5213080@gmail.com>
2023-02-20 17:53:23 +0000

    Saved progress on LargestSum 2023-02-20 17:53:23 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -1,4 +1,5 @@
 
+import java.util.Collections;
 import java.util.List;
 
 public class LargestSum {
@@ -10,22 +11,35 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        int max = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-        for (int num : nums) {
-            if (secondMax == Integer.MIN_VALUE || max == Integer.MIN_VALUE) {
-                return max;
-            }
-            else if (num > max) {
-                secondMax = max;
-                max = num;
-            }
-                 else if (num > secondMax && num < max) {
-                secondMax = num;
+        // int max = Integer.MIN_VALUE;
+        // int secondMax = Integer.MIN_VALUE;
+        // for (int num : nums) {
+        //     if (secondMax == Integer.MIN_VALUE || max == Integer.MIN_VALUE) {
+        //         return max;
+        //     }
+        //     else if (num > max) {
+        //         secondMax = max;
+        //         max = num;
+        //     }
+        //          else if (num > secondMax && num < max) {
+        //         secondMax = num;
+        //     }
+        // }
+        
+        
+        // return max + secondMax;
+        if(nums == null || nums.size() < 2) {
+            throw new IllegalArgumentException("List must contain at least two elements");
+        }
+        Collections.sort(nums, Collections.reverseOrder());
+        int largestSum = nums.get(0) + nums.get(1);
+        for(int i = 2; i < nums.size(); i++) {
+            int currentSum = nums.get(i) + nums.get(i - 1);
+            if(currentSum > largestSum) {
+                largestSum = currentSum;
             }
         }
         
-        
-        return max + secondMax;
+        return largestSum;
     }
 }
